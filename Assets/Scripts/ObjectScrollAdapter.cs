836f6ef34508e41f6b0a21a5841812674a0e435b
artek-cloud <artekguap@mail.ru>
2023-11-15 16:27:41 +0300

    Динамический список

--- Assets/Scripts/ObjectScrollAdapter.cs
+++ Assets/Scripts/ObjectScrollAdapter.cs
@@ -9,11 +9,23 @@ public class ObjectScrollAdapter : MonoBehaviour
 {
     
 public RectTransform prefab;
-public int countElements;
+private int modelsCount;
 public RectTransform content;
 public Text objectSearch;
 
 private string dbName = "URI=file:TechpriborDB.db";
+
+// Start is called before the first frame update
+void Start()
+{
+    UpdateItems();
+}
+
+// Update is called once per frame
+/*void Update()
+{
+    UpdateItems();
+}*/
 
     public void CreateDB()
     {
@@ -30,9 +42,16 @@ private string dbName = "URI=file:TechpriborDB.db";
         }
     }
 
-public void UpdateItems()
+
+    public void SearchClear()
+    {
+        objectSearch.text = "";
+        UpdateItems();
+    }
+
+    public void UpdateItems()
 {
-int modelsCount = 0;
+modelsCount = 0;
 using (var connection = new SqliteConnection(dbName))
 {
     connection.Open();
@@ -48,20 +67,75 @@ using (var connection = new SqliteConnection(dbName))
         using (IDataReader reader = command.ExecuteReader())
         {
             while (reader.Read())
+            {
             modelsCount = modelsCount + 1;
+            }
             reader.Close();
         }
     }
     connection.Close();
 }
+StartCoroutine(GetItems(modelsCount, results => OnReceivedModels(results)));
 }
 
-/*IEnumerator GetItems (int count, System.Action<ItemModel[]> callback)
+void OnReceivedModels(ItemModel[] models)
 {
+    foreach (Transform child in content)
+    {
+        Destroy(child.gameObject);
+    }
+    foreach(var model in models)
+    {
+        var instance = GameObject.Instantiate(prefab.gameObject) as GameObject;
+        instance.transform.SetParent(content, true);
+        InitializeItemView(instance, model);
+    }
+}
+
+void InitializeItemView(GameObject viewGameObject, ItemModel model)
+{
+    ItemView view = new ItemView(viewGameObject.transform);
+    view.titleText.text = model.title;
+        view.clickButton.onClick.AddListener(
+            ()=>
+            {
+                Debug.Log("Выбран " + view.titleText.text + ".");
+            }
+            );
+}
+
+IEnumerator GetItems (int count, System.Action<ItemModel[]> callback)
+{
+    yield return new WaitForSeconds(0.001f);
     var results = new ItemModel[count];
+    int i = 0;
+    using (var connection = new SqliteConnection(dbName))
+    {
+        connection.Open();
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = "select * from Objects o ";
+            if(objectSearch.text != "")
+            {
+                command.CommandText = command.CommandText + "where o.obozn like '%" + objectSearch.text + "%' ";
+            }
+            command.CommandText = command.CommandText + "order by ID;";
 
-}*/
-
+            using (IDataReader reader = command.ExecuteReader())
+            {
+                while(reader.Read())
+                {
+                    results[i] = new ItemModel();
+                    results[i].title = (reader["Obozn"].ToString());
+                    i++;
+                }
+                reader.Close();
+            }
+        }
+        connection.Close();
+    }
+    callback(results);
+}
 
 public class ItemModel
 {
@@ -71,6 +145,13 @@ public class ItemModel
 public class ItemView
 {
 public Text titleText;
-}
+        public Button clickButton;
+
+        public ItemView(Transform rootView)
+        {
+            titleText = rootView.Find("TitleText").GetComponent<Text>();
+            clickButton = rootView.Find("ClickButton").GetComponent<Button>();
+        }
+    }
 
 }
