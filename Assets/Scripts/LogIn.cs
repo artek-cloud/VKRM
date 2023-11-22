431590e415898095309f38c28176db7cc7737527
artek-cloud <artekguap@mail.ru>
2023-11-22 15:50:34 +0300

    логпас

---
+++ Assets/Scripts/LogIn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using UnityEngine.UI;
+
+public class LogIn : MonoBehaviour
+{
+
+    public InputField logIn;
+    public InputField password;
+    //для получения логина и пароля использовать logIn.text и password.text
+
+    private string servDbName = "URI=file:D:ServDB.db";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+   
+}
