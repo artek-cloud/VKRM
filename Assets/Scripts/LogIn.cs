902a56fa107b1d1babcc33866cc562f9c0f18a7e
artek-cloud <artekguap@mail.ru>
2023-11-27 15:19:13 +0300

    обработка учетных данных

--- Assets/Scripts/LogIn.cs
+++ Assets/Scripts/LogIn.cs
@@ -11,8 +11,9 @@ public class LogIn : MonoBehaviour
     public InputField logIn;
     public InputField password;
     //для получения логина и пароля использовать logIn.text и password.text
+    //поле Type таблицы Users: 0 - просмотр, 1 - администратор
 
-    private string servDbName = "URI=file:D:ServDB.db";
+    private string servDbName = "URI=file:D:/ServDB.db";
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +27,31 @@ public class LogIn : MonoBehaviour
         
     }
 
-    
+    public void LogingIn()
+    {
+        using (var connection = new SqliteConnection(servDbName))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "select * from Users u where Login = '" + logIn.text + "' and Password = '" + password.text + "'";
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        if (reader["Login"].ToString() != "")
+                            Debug.Log(reader["Login"].ToString() + " и " + reader["Password"].ToString() + " и " + reader["Name"].ToString());
+                        else
+                            Debug.Log("Данной учетной записи не существует!");
+                    }
+                    reader.Close();
+                }    
+            }
+            connection.Close();
+        }
+    }
 
    
 }
