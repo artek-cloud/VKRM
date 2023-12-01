4c6147047d5c4cfc11104a87c310aa5ea4c5b81e
artek-cloud <artekguap@mail.ru>
2023-12-01 23:06:53 +0300

    рабочий кэш, сделать пересброс кэша по кнопке

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -46,22 +46,42 @@ public class MainMenu : MonoBehaviour
                 command.ExecuteNonQuery();
                 command.CommandText = "CREATE TABLE if not exists Products (ID   INTEGER NOT NULL UNIQUE, Obozn VARCHAR(255) NOT NULL UNIQUE, Naim  VARCHAR(255) NOT NULL, PRIMARY KEY(ID AUTOINCREMENT));";
                 command.ExecuteNonQuery();
-                command.CommandText = "CREATE TABLE OPReference (ID    INTEGER NOT NULL UNIQUE, ObjectID  INTEGER NOT NULL, ProductID INTEGER NOT NULL, Kolvo INTEGER, PRIMARY KEY(ID AUTOINCREMENT), FOREIGN KEY(ProductID) REFERENCES Products(ID), FOREIGN KEY(ObjectID) REFERENCES Objects(ID));";
+                command.CommandText = "CREATE TABLE if not exists OPReference (ID    INTEGER NOT NULL UNIQUE, ObjectID  INTEGER NOT NULL, ProductID INTEGER NOT NULL, Kolvo INTEGER, PRIMARY KEY(ID AUTOINCREMENT), FOREIGN KEY(ProductID) REFERENCES Products(ID), FOREIGN KEY(ObjectID) REFERENCES Objects(ID));";
                 command.ExecuteNonQuery();
                 using (var connection2 = new SqliteConnection(servDbName))
                 {
+                    connection2.Open();
                     using (var command2 = connection2.CreateCommand())
                     {
-                        command2.CommandText = "select * from Objects;";
+                        command2.CommandText = "select ID, Obozn, Naim from Objects;";
                         using (IDataReader reader = command2.ExecuteReader())
                         {
                             while (reader.Read())
                             {
-                                command.CommandText = "insert into Objects values(" + Convert.ToInt32(reader["ID"]) + ", " + (reader["Obozn"].ToString()) + ", " + (reader["Naim"].ToString()) + ");";
+                                command.CommandText = "insert into Objects values(" + reader["ID"] + ", '" + reader["Obozn"] + "', '" + reader["Naim"] + "');";
+                                command.ExecuteNonQuery();
+                            }
+                        }
+                        command2.CommandText = "select ID, Obozn, Naim from Products;";
+                        using (IDataReader reader = command2.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                command.CommandText = "insert into Products values(" + reader["ID"] + ", '" + reader["Obozn"] + "', '" + reader["Naim"] + "');";
+                                command.ExecuteNonQuery();
+                            }
+                        }
+                        command2.CommandText = "select ID, ObjectID, ProductID, Kolvo from OPReference;";
+                        using (IDataReader reader = command2.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                command.CommandText = "insert into OPReference values(" + reader["ID"] + ", " + reader["ObjectID"] + ", " + reader["ProductID"] + ", " + reader["Kolvo"] + ");";
                                 command.ExecuteNonQuery();
                             }
                         }
                     }
+                    connection2.Close();
                 }
             }
             connection.Close();
